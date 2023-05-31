31d8f19385b1cb876dd29757d1e94993491ae37c
Arnaud WIEREL <arnaud.wierel@viacesi.fr>
2023-05-31 11:48:09 +0200

    xcwc

--- hook.php
+++ hook.php
@@ -17,11 +17,10 @@ function plugin_printercountersummary_install()
         {
         // requête de création de la table    
         $query = "CREATE TABLE `glpi_plugin_printercountersummary_profiles` (
-                `id` int(11) UNSIGNED NOT NULL default '0' COMMENT 'RELATION to glpi_profiles (id)',
-                `right` char(1) collate utf8_unicode_ci default NULL,
-                PRIMARY KEY  (`id`)
-            ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
-
+            `id` int(11) UNSIGNED NOT NULL default '0' COMMENT 'RELATION to glpi_profiles (id)',
+            `right` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci default NULL,
+            PRIMARY KEY  (`id`)
+          ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci";
 
         $DB->query($query) or die($DB->error());
 
