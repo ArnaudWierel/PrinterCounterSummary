ec66565599009b21f2344d84169d57e1e708717f
Arnaud WIEREL <arnaud.wierel@viacesi.fr>
2023-06-01 15:38:56 +0200

    essaie réponse chatgpt

--- front/printercountersummary.php
+++ front/printercountersummary.php
@@ -1,9 +1,14 @@
 <?php
 
-//include ("../../../inc/includes.php");
+include ("../../../inc/includes.php");
 
-Html::header(__("Printer Counter Summary", "printercountersummary"), $_SERVER['PHP_SELF'], "plugins",
-             "printercountersummary", "printercountersummary");
+Html::header(
+    __("Printer Counter Summary", "printercountersummary"), 
+    $_SERVER['PHP_SELF'], 
+    "plugins", 
+    "printercountersummary", 
+    "printercountersummary"
+);
 
 echo '<div class="center">';
 echo '<h2>' . __('Welcome to the Printer Counter Summary plugin!', 'printercountersummary') . '</h2>';
