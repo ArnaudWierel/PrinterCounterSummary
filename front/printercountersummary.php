a8394a2878ec36a19e219c2198ce09623814341c
Arnaud WIEREL <arnaud.wierel@viacesi.fr>
2023-06-01 15:29:47 +0200

    ajout du contenue pour la page du plugin

--- front/printercountersummary.php
+++ front/printercountersummary.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "test";
+include ("../../../inc/includes.php");
+
+Html::header(__("Printer Counter Summary", "printercountersummary"), $_SERVER['PHP_SELF'], "plugins",
+             "printercountersummary", "printercountersummary");
+
+echo '<div class="center">';
+echo '<h2>' . __('Welcome to the Printer Counter Summary plugin!', 'printercountersummary') . '</h2>';
+echo '</div>';
+
+Html::footer();
