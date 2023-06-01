974da1bd4b46f9f271a1a85404b851bc08568598
Arnaud WIEREL <arnaud.wierel@viacesi.fr>
2023-06-01 15:20:42 +0200

    test pour la page du plugin

--- front/printercountersummary.php
+++ front/printercountersummary.php
@@ -1,11 +1,4 @@
 <?php
 
 include ("../../../inc/includes.php");
-
-Html::header(__("Printer Counter Summary", "printercountersummary"), $_SERVER['PHP_SELF'], "plugins","printercountersummary", "printercountersummary");
-
-echo '<div class="center">';
-echo '<h2>' . __('Welcome to the Printer Counter Summary plugin!', 'printercountersummary') . '</h2>';
-echo '</div>';
-
-Html::footer();
+echo "test";
