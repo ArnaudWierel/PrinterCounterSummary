8206fccf7690e687dd349eafeb2ccc13666e3f03
ArnaudWierel <arnaud.67210@gmail.com>
2023-06-02 14:36:52 +0200

    enfinnn la page fonctionnne

--- front/printercountersummary.php
+++ front/printercountersummary.php
@@ -1,17 +1,16 @@
 <?php
+// glpi page for the plugin
+// Path: front\printercountersummary.php
+include ('../../../inc/includes.php');
 
-include ("../../../inc/includes.php");
 
-Html::header(
-    __("Printer Counter Summary", "printercountersummary"), 
-    $_SERVER['PHP_SELF'], 
-    "plugins", 
-    "printercountersummary", 
-    "printercountersummary"
-);
+// include the header of the page
+Html::header(__('Printer Counter Summary', 'printercountersummary'), $_SERVER['PHP_SELF'], "tools", "plugin_printercountersummary");
 
+// include the a title for the page in order to test the plugin
 echo '<div class="center">';
-echo '<h2>' . __('Welcome to the Printer Counter Summary plugin!', 'printercountersummary') . '</h2>';
+echo '<h2>Welcome to the Printer Counter Summary plugin!  ', 'Printer Counter Summary</h2>';
 echo '</div>';
 
+// include the footer of the page
 Html::footer();
--- inc/printercountersummary.class.php
+++ inc/printercountersummary.class.php
@@ -5,13 +5,40 @@ if (!defined('GLPI_ROOT')) {
 }
 
 class PluginPrinterCounterSummary extends CommonDBTM {
+    
+    static $rightname = 'plugin_printercountersummary';
+
+    static function getTypeName($nb = 0) {
+      return __('Printer Counter Summary', 'printercountersummary');
+   }
+
+    //showmenu with the echo
+    static function showMenu() {
+        echo '<div class="center">';
+        echo '<h2>Welcome to the Printer Counter Summary plugin!', 'printercountersummary</h2>';
+        echo '</div>';
+    }
+
     static function getMenuContent() {
         return array(
-            'title' => __("Printer Counter Summary", 'printercountersummary'),
+            'title' => self::getTypeName(2),
             'page'  => "/plugins/PrinterCounterSummary/front/printercountersummary.php",
-            'icon'  => 'printercountersummary',
+            'icon'  => 'fas fa-print',
             'menu'  => 'plugins'
         );
     }
-}
 
+    // (Optionally) implement access rights 
+    function getRights($interface = 'central') {
+        switch ($interface) {
+            case 'central':
+                return [
+                    'plugin_printercountersummary' => 'PluginPrinterCounterSummary'
+                ];
+            case 'profile':
+                return [
+                    'plugin_printercountersummary' => 'PluginPrinterCounterSummary'
+                ];
+        }
+    }
+}
