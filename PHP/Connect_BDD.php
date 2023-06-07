776cc7c82efdf6e6c2f914356004451778e5662a
Arnaud WIEREL <arnaud.wierel@viacesi.fr>
2023-06-07 16:58:00 +0200

    yyess

--- PHP/Connect_BDD.php
+++ PHP/Connect_BDD.php
@@ -6,10 +6,14 @@ $pass = 'Sbx3390rds!';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$opt = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
+$options = [
+   PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+   PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+   PDO::ATTR_EMULATE_PREPARES   => false,
 ];
-$pdo = new PDO($dsn, $user, $pass, $opt);
+try {
+   $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+   throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
 ?>
