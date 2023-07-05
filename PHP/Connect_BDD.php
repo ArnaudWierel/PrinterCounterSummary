eff852fb9ca92b38b7e321c1c8fb4f2cb3c894af
ArnaudWierel <arnaud.67210@gmail.com>
2023-07-05 14:51:56 +0200

    dfdf

--- PHP/Connect_BDD.php
+++ PHP/Connect_BDD.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$db   = 'glpigroupe';
+$db   = 'glpi';//glpigroupe
 $user = 'root';
-$pass = 'Sbx3390rds!';
+$pass = ''; //Sbx3390rds!
 $charset = 'utf8mb4';
 
 // Configuration des options de connexion
