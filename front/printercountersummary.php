eff5e7375e1e04320bf673ba183600f9c9c1def2
ArnaudWierel <arnaud.67210@gmail.com>
2023-08-29 09:23:24 +0200

    on teste

--- front/printercountersummary.php
+++ front/printercountersummary.php
@@ -56,7 +56,11 @@ foreach ($values as $value) {
     $imprimanteId = $value['id'];
     $imprimanteItemId = $value['items_id'];
     echo '<tr>';
-    echo '<td><a href="http://10.67.100.111/glpi/plugins/PrinterCounterSummary/front/ShowMonthCons.php?id=' . $value['id'] . '">' . $names['name'] . '</a></td>';
+    echo '<td><a href="http://10.67.100.111/glpi/plugins/PrinterCounterSummary/front/ShowMonthCons.php?id=' . $value['id'] . '">';
+    foreach ($names as $name) {
+        echo $name['name'];
+    }
+    echo '</a></td>';
 
     $ipAddress = $ipAdress->getIPByPrinterId($imprimanteItemId);
     echo '<td><a href="http://' . $ipAddress . '/">' . $ipAddress . '</a></td>';
