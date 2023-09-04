87841f3dc54070a7cee0cdacfc087bf3c2d9950c
ArnaudWierel <arnaud.67210@gmail.com>
2023-09-04 11:04:10 +0200

    dfsdfsdf

--- inc/CalMoisCons.class.php
+++ inc/CalMoisCons.class.php
@@ -21,7 +21,7 @@ class CalMoisCons
     private $compteur_name;
     private $compteur;
     private $id_relevé_1;
-    public $pdo;
+    private $pdo;
     private $id_relevé_2;
     private $imprimante_id_2;
     private $imprimante_name_2;
